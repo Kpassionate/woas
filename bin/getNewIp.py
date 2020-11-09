65263b5e5d3b97cee10e3cbb52949a40608ebfa7
super-GY <guanguanjujiu2018@163.com>
2020-11-09 16:49:18 +0800

    修改获取ip的脚本

--- bin/getNewIp.py
+++ bin/getNewIp.py
@@ -1,39 +1,38 @@
 # -*- coding: utf-8 -*-
-from urllib import parse, request
 
 __author__ = 'yankai.guan'
 import socket
 import time
+from urllib import parse, request
 
 
 def get_ip():
-    exip = ''
-    ipList = socket.gethostbyname_ex(socket.gethostname())
-
-    for itemList in ipList:
+    ip_list = socket.gethostbyname_ex(socket.gethostname())
+    for itemList in ip_list:
         if isinstance(itemList, list):
             for ip in itemList:
-                print('####:', ip)
                 if ip not in ['192.168.11.58']:
-                    exip = ip
-    return exip
+                    save_ip(ip)
 
 
 def save_ip(exip):
     if exip:
-        params = parse.urlencode({'host': exip, 'port': 808})
-        f = request.urlopen("http://wechatspider.0fenbei.com/wechat/proxy/1/edit/", params)
-        print(f.read())
+        print("new ip:", exip)
+        params = parse.urlencode({'host': exip, 'port': 8000}).encode('utf-8')
+        f = request.urlopen("http://127.0.0.1:8000/wechat/proxy/1/edit/", params)
+        f.read()
+    print("休息3秒")
+    time.sleep(3)
 
 
 if __name__ == '__main__':
+    """
+    每3秒修改一次host,以防止被禁
+    """
     while True:
         try:
-            exip = get_ip()
-            print('new ip:', exip)
-            save_ip(exip)
+            get_ip()
         except Exception as e:
-            print(e, "aaa")
+            print(e)
         finally:
-            print('sleep 1 second')
-            time.sleep(1)
+            print("重新更换！")
